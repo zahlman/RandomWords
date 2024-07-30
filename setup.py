5118af40e127ff88124c2f81304a456476cd1826
Karl Knechtel <zahlman@proton.me>
2024-07-30 19:35:10 -0400

    Check for error importing setuptools.command.test
    In Setuptools 72.0.0, `setuptools.command.test` was temporarily removed.
    This version of Setuptools was yanked, and 72.1.0 restored the
    functionality (it appears that the removal didn't follow the standard
    process, but this functionality has been deprecated for many years).
    
    The functionality will be removed in a future version (probably very
    soon) and that would break this `setup.py` as a result.
    
    This commit, as a first step, handles the `ImportError` that results
    when `setuptools.command.test` is removed, and only attempts to define
    the `PyTest` command (the only thing depending on it) when it's
    available.
    
    For more details, please see:
    
    https://github.com/pypa/setuptools/issues/4519
    
    Although many projects were affected by the change, this one was the
    unlucky "winner" of a popular Stack Overflow Q&A drawing attention to
    the general issue:
    
    https://stackoverflow.com/questions/78806100

--- setup.py
+++ setup.py
@@ -3,19 +3,25 @@
 import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
+try:
+    from setuptools.command.test import test as TestCommand
+except ImportError:
+    # setuptools.command.test has been removed.
+    # Since we can't define `PyTest`, there are no `cmdclass`es to use.
+    CMDCLASS = {}
+else:
+    class PyTest(TestCommand):
+        def finalize_options(self):
+            TestCommand.finalize_options(self)
+            self.test_args = ['--strict-markers', '--verbose', '--tb=long']
+            self.test_suite = True
 
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = ['--strict-markers', '--verbose', '--tb=long']
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
+        def run_tests(self):
+            import pytest
+            errcode = pytest.main(self.test_args)
+            sys.exit(errcode)
+    # Use `PyTest` as a `cmdclass`.
+    CMDCLASS = {'test': PyTest}
 
 
 setup(
@@ -43,9 +49,7 @@ setup(
     include_package_data=True,
     install_requires=[],
     tests_require=['pytest'],
-    cmdclass={'test': PyTest},
+    cmdclass=CMDCLASS,
     test_suite='random_words.test.test_random_words',
-    extras_require={
-         'testing': ['pytest'],
-     },
+    extras_require={ 'testing': ['pytest'] },
 )
