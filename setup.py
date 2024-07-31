7e4b5eca924c7e73ae50f2a5208c6e6ccc02d3a2
Karl Knechtel <zahlman@proton.me>
2024-07-30 20:12:32 -0400

    Refactoring
    With the modern approach using `pyproject.toml`, it's not actually
    necessary to call `setup` to build the project, as long as all necessary
    information for wheel building is in `pyproject.toml` (which it is now).
    
    Therefore, the script now immediately bails out whenever
    `setuptools.command.test` is not available, and uses a minimal `setup`
    invocation when it is.
    
    Going forward, it is strongly recommended to remove `setup.py` entirely.
    A `setup.py` file is only required for projects that need to build
    extensions or have some other compelling reason to execute arbitrary
    code at build time. The interface implemented here - i.e. calling
    `setup.py test` - is deprecated, and has been for many years.
    
    Instead, consider using a shell alias to make the desired command (i.e.
    `python -m pytest --strict-markers --verbose --tb=long`) easier to use.
    
    For more details, please see:
    https://setuptools.pypa.io/en/latest/deprecated/commands.html

--- setup.py
+++ setup.py
@@ -7,26 +7,28 @@ try:
     from setuptools.command.test import test as TestCommand
 except ImportError:
     # setuptools.command.test has been removed.
-    # Since we can't define `PyTest`, there are no `cmdclass`es to use.
-    CMDCLASS = {}
-else:
-    class PyTest(TestCommand):
-        def finalize_options(self):
-            TestCommand.finalize_options(self)
-            self.test_args = ['--strict-markers', '--verbose', '--tb=long']
-            self.test_suite = True
-
-        def run_tests(self):
-            import pytest
-            errcode = pytest.main(self.test_args)
-            sys.exit(errcode)
-    # Use `PyTest` as a `cmdclass`.
-    CMDCLASS = {'test': PyTest}
+    # The `setup` call is only necessary when defining this `cmdclass`, since
+    # building the project is no longer dependend on that.
+    # Therefore, there's no more to do here.
+    sys.exit()
 
 
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict-markers', '--verbose', '--tb=long']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+# Use `PyTest` as a `cmdclass`.
 setup(
     tests_require=['pytest'],
-    cmdclass=CMDCLASS,
+    cmdclass={'test': PyTest},
     test_suite='random_words.test.test_random_words',
-    extras_require={ 'testing': ['pytest'] },
+    extras_require={ 'testing': ['pytest'] }
 )
